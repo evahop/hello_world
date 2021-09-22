27b14bbfeb664bd82a2bd2aa5dea029571c3863e
Evan Mark Hopkins <git@evahop.com>
2021-09-21 22:47:21 -0700

    clears canvas

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+const adapter = await navigator.gpu.requestAdapter()
+const device = await adapter.requestDevice()
+
+const canvas = document.createElement('canvas')
+document.body.appendChild(canvas)
+
+const context = canvas.getContext('webgpu')
+const format = context.getPreferredFormat(adapter)
+context.configure({ device, format })
+
+while (true) {
+  await new Promise(requestAnimationFrame)
+  const encoder = device.createCommandEncoder()
+  const pass = encoder.beginRenderPass({
+    colorAttachments: [{
+      view: context.getCurrentTexture().createView(),
+      loadValue: [0, 0, 0, 1],
+      storeOp: 'discard'
+    }]
+  })
+  pass.endPass()
+  device.queue.submit([encoder.finish()])
+}
